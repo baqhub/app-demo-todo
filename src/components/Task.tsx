095addfb5dc26f8b20f3bf63c9ae7ce867d47220
Quentin Calvez <quentin.calvez@quentez.com>
2024-03-31 01:56:43 +0100

    Connect components.

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,15 +1,33 @@
-import {FC} from "react";
+import {ChangeEvent, FC} from "react";
+import {Record} from "@baqhub/sdk";
+import {useRecordByKey, useRecordHelpers} from "../baq/store";
+import {TaskRecord, TaskRecordKey} from "../baq/taskRecord";
+
 
 interface TaskProps {
-  title: string;
-  completed: boolean;
+  taskKey: TaskRecordKey;
 }
 
 export const Task: FC<TaskProps> = props => {
-  const {title, completed} = props;
+  const {taskKey} = props;
+  const {entity, updateRecords} = useRecordHelpers();
 
-  const onCompletedChange = () => console.log("Completed!");
-  const onDeleteClick = () => console.log("Deleted!");
+  // Find the requested record.
+  const record = useRecordByKey(taskKey);
+  const {title, completed} = record.content;
+
+  const onCompletedChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const updatedRecord = TaskRecord.update(entity, record, {
+      ...record.content,
+      completed: e.currentTarget.checked,
+    });
+    updateRecords([updatedRecord]);
+  };
+
+  const onDeleteClick = () => {
+    const deletedRecord = Record.delete(entity, record);
+    updateRecords([deletedRecord]);
+  };
 
   return (
     <li className={completed ? "completed" : undefined}>
